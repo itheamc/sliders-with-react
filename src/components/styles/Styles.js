7eb03aa5082d8d21ae4edeead2ad0f655a905c90
Amit Chaudhary <itheamc@gmail.com>
2021-03-11 09:08:24 +0545

    minor improvement in slider

--- src/components/styles/Styles.js
+++ src/components/styles/Styles.js
@@ -17,23 +17,49 @@ export const Sliders = styled.div`
     /* background-color: red; */
 `
 
-// Animation
-const enteranim = keyframes`
-    /* from {
+// Animation for slider -- left to right
+const leftenter = keyframes`
+    0% {
+        opacity: 0;
         left: -20%;
     }
-    to {
-        left: 0;
-    } */
-
-    0% {
-        left: -20%;
+    25% {
+        opacity: 0.25;
     }
     50% {
+        opacity: 0.5;
         left: 20%;
         right: -20%
     }
+    75% {
+        opacity: 0.75;
+    }
     100% {
+        opacity: 1;
+        left: 0;
+        right: 0;
+    }
+`
+
+// Animation for slider -- right to left
+const rightenter = keyframes`
+    0% {
+        opacity: 0;
+        right: -40%;
+    }
+    25% {
+        opacity: 0.25;
+    }
+    50% {
+        opacity: 0.5;
+        left: -20%;
+        right: 20%;
+    }
+    75% {
+        opacity: 0.75;
+    }
+    100% {
+        opacity: 1;
         left: 0;
         right: 0;
     }
@@ -50,14 +76,24 @@ export const Slider = styled.div`
     text-align:center;
     flex-wrap: wrap;
     background-color: ${({bgColor}) => bgColor || 'white'};
-    animation: ${enteranim} 2s cubic-bezier(0.785, 0.135, 0.15, 0.86);
+    animation: ${leftenter} 2s linear;
     /* transition: all 2s cubic-bezier(0.785, 0.135, 0.15, 0.86); */
 `
 
+// Animation for image slider
+const visibility = keyframes`
+    from{
+        opacity: 0;
+    }
+    to {
+        opacity: 1;
+    }
+`
 
 export const SliderImage = styled.img`
     width: 100%;
     height: 100%;
+    animation: ${visibility} 1s linear;
     /* src: url(); */
 `
 
@@ -81,7 +117,7 @@ export const Indicator = styled.span`
     position: relative;
     box-sizing: border-box;
     margin: 2px 2px;
-    transition: all 1s ease;
+    transition: all 1.5s ease;
 
     &:hover{
         cursor: pointer;
